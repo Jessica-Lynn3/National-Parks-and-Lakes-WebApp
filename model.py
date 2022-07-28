026d175760de7580f8799e6f58128ea2f70ecfd9
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-07-27 17:49:03 -0700

    In model.py: Able to create User and Place objects and query for them sucessfully

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 """ Models for National Parks and Lakes App """
 
-#importing SQLAlchemy
-from doctest import debug_script
+#importing datetime and SQLAlchemy
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 #calling SQLAlchemy constructor function to create a SQLAlchemy instance at the variable db
 db = SQLAlchemy()
 
-#name of database: destinations_db
+#name of PostgreSQL database: destinations_db
 
 #Create User Class
 class User(db.Model):
@@ -20,15 +20,103 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
+    user_top_places = db.relationship('User_Top_Place', back_populates="users") 
+    notes = db.relationship('User_Note', back_populates="note_by_user") 
+
+
     def __repr__(self):
-        return f"<User user_id={self.user_id}, Username username={self.username}, email={self.email}, password={self.password}>"
+        return f"<User -- user_id={self.user_id}, Username username={self.username}, email={self.email}, password={self.password}>"
 
 
 #Create Place Class
+class Place(db.Model):
+    """ A place - can be a National Park or a Lake.
+    
+    Note:   A lake is a place that is not listed as an official National Park.
+            A lake is an item in a collection of the Top Ten Largest Lakes in the US.  """
+
+    __tablename__ = "places"
+
+    place_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    external_id = db.Column(db.Integer)
+    is_park = db.Column(db.Boolean, nullable=False)
+    is_lake = db.Column(db.Boolean, nullable=False)
+
+    place_name = db.Column(db.Text, nullable=False)
+    photo = db.Column(db.Text)
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+    state_location = db.Column(db.String)
+
+    website = db.Column(db.Text)
+    phone_contact = db.Column(db.BigInteger)
+    opens_at = db.Column(db.DateTime)
+    closes_at = db.Column(db.DateTime)
+
+    fee_info = db.Column(db.Text)
+    reservation_required = db.Column(db.Boolean)
+    overview = db.Column(db.Text)
+    highlighted_features = db.Column(db.Text)
+
+    trails_easy = db.Column(db.Boolean)
+    trails_medium = db.Column(db.Boolean)
+    trails_hard = db.Column(db.Boolean)
+    trails_dog_friendly = db.Column(db.Boolean)
+    trails_accessible = db.Column(db.Boolean)
+
+    beaches_dog_friendly = db.Column(db.Boolean)
+    beaches_accessible = db.Column(db.Boolean)
+    swimming_allowed = db.Column(db.Boolean)
+    boating_allowed = db.Column(db.Boolean)
+    beach_open_to_public = db.Column(db.Boolean)
+
+    camping_allowed = db.Column(db.Boolean)
+    camper_rv_allowed = db.Column(db.Boolean)
+
+    landscape_photography_allowed = db.Column(db.Boolean)
+    season_to_visit = db.Column(db.String)
+
+    user_top_places = db.relationship('User_Top_Place', back_populates="places") 
+    place_note = db.relationship('User_Note', back_populates="user_note_place") 
+
+    def __repr__(self):
+        return f"<Place -- place_name={self.place_name}, -- Park? is_park={self.is_park}, -- Lake? is_lake={self.is_lake}>"
+
 
 #Create User Top Place Class
+class User_Top_Place(db.Model):
+    """ A User-Rated Top Place """
+
+    __tablename__ = "user_top_places"
+
+    user_top_place_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    place_id = db.Column(db.Integer, db.ForeignKey('places.place_id'))
+
+    users = db.relationship('User', back_populates="user_top_places") 
+    places = db.relationship('Place', back_populates="user_top_places") 
+
+    def __repr__(self):
+        return f"<User Top Place -- user_id={self.user_id}, place_id={self.place_id}>"
+
 
 #Create User Note Class
+class User_Note(db.Model):
+    """ A User Note for a Place """
+
+    __tablename__ = "user_notes"
+
+    note_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    place_id = db.Column(db.Integer, db.ForeignKey('places.place_id'))
+    note = db.Column(db.Text)
+
+    note_by_user = db.relationship('User', back_populates="notes") 
+    user_note_place = db.relationship('Place', back_populates="place_note") 
+
+    def __repr__(self):
+        return f"<User Note -- user_id={self.user_id}, place_id={self.place_id}>"
+
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///destinations_db", echo=True):
