6d4fee4a6dee6d3c39a816b2dc17e81b47e8e092
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-08-24 20:45:21 -0700

    Added comment on line 88

--- model.py
+++ model.py
@@ -85,7 +85,7 @@ class UserNote(db.Model):
 
 def connect_to_db(flask_app, db_uri="postgresql:///parks_db", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri #the location of database
-    flask_app.config["SQLALCHEMY_ECHO"] = False
+    flask_app.config["SQLALCHEMY_ECHO"] = False  #set to False for now; can change back to echo later
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 
     db.app = flask_app 
