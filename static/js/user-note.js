b7a05dadf9f3abd8aacc0888e52880b77a5734bd
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-09-13 23:03:14 -0700

    Added fetch for new route /show-all-notes -- displays all user notes on place-page.html

--- static/js/user-note.js
+++ static/js/user-note.js
@@ -1,35 +1,46 @@
 "use strict";
 
+// making the park code a global variable 
+const PARK_CODE = document.querySelector('#park-code').value;
+
+// when window loads call the fetchUserParkNotes function
+window.onload = fetchUserParkNotes; 
+
+function fetchUserParkNotes() {
+  fetch(`/show-all-notes/${PARK_CODE}`)
+  .then((response) => response.json()) 
+  .then((responseJson) => {
+    const userNotes = responseJson.data.reduce((result, note) => {
+      result += `<li>${note}</li>`;
+      return result;
+    }, '');
+    document.querySelector('#noteDisplay').innerHTML = userNotes;
+  })
+};
+
 //for user note
 // use fetch POST request
 
-document.querySelector('#save-park-note').addEventListener('click', (evt) => {
-    evt.preventDefault();
-    alert('Victory!');
+document.querySelector('#save-park-note').addEventListener('click', saveNote)
 
+function saveNote() {
+//   alert('Victory!');
   // ajax call-> find the script in textbox and assign it to a variable. 
   // ajax call -> will send that variable, to route on the backend. 
   // whatever is returned in your server route -> will be returned to your ajax call
-// document.queryselector. (whatever the div is) .inner text == response. 
-    const park_code= document.querySelector('#park-code').value;
+  // document.queryselector. (whatever the div is) .inner text == response. 
 
-    const formInput = {
-        note: document.querySelector('#park-note').value        
-    };
-    console.log(park_code);
-    fetch(`/save-note/${park_code}`, {
-        method: 'POST',
-        body: JSON.stringify(formInput),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then((response) => response.json())
-        .then((responseJson) => {
-            // responseJson is the response-- gives me back returned object from this route: jsonify(note_from_db.note)
-            document.querySelector('#noteDisplay').innerText = responseJson;
-        });
-});
+  const formInput = {
+      note: document.querySelector('#park-note').value        
+  };
 
-
-
+  fetch(`/save-note/${PARK_CODE}`, {
+      method: 'POST',
+      body: JSON.stringify(formInput),
+      headers: {
+          'Content-Type': 'application/json',
+      },
+  })
+  .then((response) => response.json())
+  .then((responseJson) => console.log(responseJson))
+};
